d68d825898ae7ecad85326eb420d8a15a7cecb24
NiXerasse <nixerasse@gmail.com>
2023-10-22 00:33:06 +0300

    Receiving data from terminals

--- app/listener/routes.py
+++ app/listener/routes.py
@@ -1,5 +1,6 @@
 from app.listener import views
 
+
 def setup_routes(app):
     app.router.add_get("/", views.index)
     app.router.add_post("/", views.post)
--- app/listener/views.py
+++ app/listener/views.py
@@ -1,7 +1,15 @@
 from aiohttp import web
+import json
 
-async def index(request):
-    return web.Response(text="This is a test app!")
 
-async def post(request):
-    print(f"Request: {request}")
+async def index(request: web.Request):
+    return web.Response(text="This is http-listener service for FaceID system.")
+
+
+async def post(request: web.Request):
+    content: bytes = await request.content.read()
+    json_data: dict = json.loads(content[69:-21].decode().replace('\n', '').replace('\t', ''))
+    terminal_name: str = json_data['AccessControllerEvent']['deviceName']
+    terminal_mac: str = json_data['macAddress']
+    terminal_ip: str = json_data['ipAddress']
+    print(f'Terminal: {terminal_name} -> {terminal_ip} ({terminal_mac})')
