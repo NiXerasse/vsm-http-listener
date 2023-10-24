69960276ab473f990669c282c7258af58ae3c5e4
NiXerasse <nixerasse@gmail.com>
2023-10-24 17:47:47 +0300

    added totals to inmemorydb __str__

--- app/store/database/inmemorydb.py
+++ app/store/database/inmemorydb.py
@@ -15,15 +15,15 @@ class InmemoryDB:
         self.db = {}
 
     def add(self, data: "TerminalData"):
-        if data.ip in self.db:
-            if self.db[data.ip].name != data.name or self.db[data.ip].mac != data.mac:
+        if data.mac in self.db:
+            if self.db[data.mac].name != data.name or self.db[data.mac].ip != data.ip:
                 logger.info(f'Updated terminal: {data.name} {data.ip} {data.mac}')
         else:
             logger.info(f'Added terminal: {data.name} {data.ip} {data.mac}')
-        self.db[data.ip] = data
+        self.db[data.mac] = data
 
     def __str__(self):
         return '\n'.join(
             f'{terminal.name.ljust(20, " ")}{terminal.ip.rjust(15, " ")}    {terminal.mac}'
             for terminal in sorted(self.db.values(), key=lambda d: d.name)
-        )
+        ) + '\n' + f'Total number of terminals: {len(self.db)}'.rjust(56, ' ')
