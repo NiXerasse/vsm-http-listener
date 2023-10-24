fd8fe7d30172a830e3e6aa639d418ce5726c0731
NiXerasse <nixerasse@gmail.com>
2023-10-24 17:35:29 +0300

    added new route

--- app/listener/routes.py
+++ app/listener/routes.py
@@ -4,3 +4,4 @@ from app.listener import views
 def setup_routes(app):
     app.router.add_get("/", views.index)
     app.router.add_post("/", views.post)
+    app.router.add_post("/ISAPI/Event/notification/httpHosts", views.post)
